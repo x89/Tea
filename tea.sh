403abe8890bf2cecc7494c027884fc0bb23963fc
x89 <napalm10@gmail.com>
2015-05-09 11:16:36 +0100

    Working tea.sh

--- tea.sh
+++ tea.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
-sleep 2
-mpv tea.flac
+
+if [ -r "tea.flac" ]; then
+    tea_file="tea.flac"
+elif [ -r "tea.mp3" ]; then
+    tea_file="tea.mp3"
+else
+    echo "EMERGENCY!!! YOU WILL GET NO TEA WARNING"
+fi
+
+echo "You are using $tea_file."
+echo "Have no fear! None! you will be warned when your tea is ready."
+
+sleep 1  # 5 minutes
+
+mpv $tea_file &>/dev/null || \
+mplayer -nogui $tea_file &>/dev/null\
+vlc -I dummy $tea_file &>/dev/null #  Feel free to add more! 
+
