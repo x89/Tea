3f47aff11034e381ff7ce3f97c3dbf5efe0ccc94
x89 <napalm10@gmail.com>
2015-05-11 14:30:30 +0100

    Changes from ajikeshi: Merged teatimer.sh and tea.sh and cleaned up a bit

--- tea.sh
+++ tea.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-if [ -r "tea.flac" ]; then
-    tea_file="tea.flac"
-elif [ -r "tea.mp3" ]; then
-    tea_file="tea.mp3"
-else
-    echo "EMERGENCY!!! YOU WILL GET NO TEA WARNING"
-    echo "(this is because we couldn't find a sound file for you)"
-    exit
-fi
 
+<<<<<<< HEAD
 tea_time=6m #  6 minutes (as per ISO 3103)
 
 while [ "$1" != "" ]; do
@@ -19,11 +11,36 @@ while [ "$1" != "" ]; do
     fi
     shift
 done        
+=======
+PrintUsage(){
+echo "Defaults:"
+echo "Brew 5 minutes with milk"
+echo ""
+echo "Options:"
+echo "-t ISO 3103 tea"
+echo "-s short brewing tea (i.e. green tea)"
+echo "-l long brewing tea (i.e. herbal tea)"
+echo "-m without milk"
+echo "-c custom brewing time in seconds"
+echo "-h or --help prints this"
+>>>>>>> eb9b01a9123f23fd6f1444b1016f7f077e77066b
 
-echo "You are using $tea_file."
-echo "Have no fear! None! You will be warned when your tea is ready."
+exit 1
+}
 
-sleep "$tea_time" 
+PrintDone(){ # JUST SOME ASCII FLUFF
+echo ""
+echo "   {  {     "
+echo "    }  }    "
+echo "  ,{--{-.   "
+echo " (  }    )  "
+echo " |\`-----´|_ "
+echo " |       | \\ "
+echo " |  Tea  | |"
+echo " |       |_/"
+echo " \\       /  "
+echo "  \`-----´   "
+}
 
 commandpicker() {
     # Thanks to @moopie - github for this function
@@ -36,8 +53,6 @@ commandpicker() {
     fi
 }
 
-commandpicker
-
 write_terminal() {
     for u in $(who | grep "^${USER} " | awk '{print $1":"$2}'); do
         a=$(echo "${u}" | cut -d: -f1,1)
@@ -46,9 +61,109 @@ write_terminal() {
     done
 }
 
-$player $tea_file &>/dev/null || \
+
+# Check for audio files
+if [ -r "tea.flac" ]; then
+    tea_file="tea.flac"
+elif [ -r "tea.mp3" ]; then
+    tea_file="tea.mp3"
+else
+    echo "EMERGENCY!!! YOU WILL GET NO TEA WARNING"
+    echo "(this is because we couldn't find a sound file for you)"
+    exit
+fi
+
+commandpicker
+
+teatime=3 #00 #  5 minute default tea
+
+echo ""
+while test $# -gt 0
+do
+case "$1" in
+
+    -h)
+        teahelp=1
+    ;;
+    
+
+    --help)
+        teahelp=1
+    ;;
+
+    -t)
+        teatime=3 #60
+    ;;
+
+    -l)
+        teatime=4 #80
+    ;;
+
+    -s)
+        teatime=2 #40
+    ;;
+
+    -m)
+        milk=1;
+    ;;
+
+    -c)
+        if [ $2 -gt 0 ] 2>/dev/null ; then
+            teatime=$2
+            echo $teatime
+        else
+            PrintUsage
+        fi
+
+    ;;
+
+    0)  ;; # to catch any number, todo: move into -c option
+    1)  ;;
+    2)  ;;
+    3)  ;;
+    4)  ;;
+    5)  ;;
+    6)  ;;
+    7)  ;;
+    8)  ;;
+    9)  ;;
+
+     
+    -*)
+        echo "this option does not exist: $1"
+        teahelp=1
+    ;;
+
+    *)
+    ;;
+
+esac
+shift
+done
+
+if [ ! -z $teahelp ]; then
+    if [ $teahelp -eq 1 ]; then
+        PrintUsage
+        exit 1
+    fi
+fi
+
+echo "You are using $tea_file."
+echo "Have no fear! None! You will be warned when your tea is ready."
+sleep "$teatime"
+
+$player $tea_file 1>&- 2>&- || \
 echo "We couldn't play your tea file." &>/dev/null
-echo "BUT YOUR TEA IS NOW READY, PUT THE MILK IN NOW!!!!"
+
+
+
+if [ -z $milk ]; then
+    PrintDone
+    echo "BUT YOUR TEA IS NOW READY, PUT THE MILK IN NOW!!!!"
+elif [ ! -z $milk ]; then
+    PrintDone
+    echo "BUT YOUR TEA IS NOW READY."
+fi
 
 write_terminal "tea.sh: Your tea is ready!"
 
